710a192ed0125cdd018309b7341922b7a05372cb
Matic Vertačnik <matic.vertacnik@icloud.com>
2025-10-08 21:56:13 +0200

    Add documentation for DAMP features and installation guide

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,12 +1,19 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
+import * as FilesComponents from 'fumadocs-ui/components/files';
+import * as TabsComponents from 'fumadocs-ui/components/tabs';
 import type { MDXComponents } from 'mdx/types';
-import * as icons from 'lucide-react';  // Import all icons from lucide-react
+import { Accordion, Accordions } from 'fumadocs-ui/components/accordion';
+import * as icons from 'lucide-react'; // Import all icons from lucide-react
 
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...(icons as unknown as MDXComponents),
     ...defaultMdxComponents,
+    ...TabsComponents,
+    ...FilesComponents,
+    Accordion,
+    Accordions,
     ...components,
   };
 }
